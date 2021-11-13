bef2ac3816bedb96f0d133a2730de0e63c9ce5e8
Saptrey <mr.kostya.kapitonov@mail.ru>
2021-11-14 02:11:16 +0300

    add main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ def hello_world():
 
 
 #That's my first try on Github and I don't want any of critic ;)
+def f(x):
+    s = 1
+    for i in range(1,x+1):
+        s = s * i
+
+    return s
+
+print(f(5))
